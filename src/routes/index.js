318a68fa179f379a841e85c053ab884b710ed0f5
Senan <senan996@gmail.com>
2021-11-01 17:09:36 +0100

    Pruebas de inicio

---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+
+// Modelos
+const Usuario = require('../models/usuarios');
+
+router.get('/', async(req, res) => {
+    const usuarios = await Usuario.find();
+    res.render('index.html', {usuarios, title: 'CRUD de usuarios'}); // Accede a src/viwes y busca index.html
+});
+
+router.post('/add', async (req, res) => {
+    const usuario = new Usuario(req.body); // Añade los datos de la petición en el objeto usuario, y le añade el _id de MongoDB
+
+    await usuario.save();
+    res.redirect('/');
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params; // Es lo mismo que hacer const id = req.params.id -> Usamos deconstrucción por buena práctica
+    await Usuario.deleteOne({_id: id});
+    
+    res.redirect('/');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const usuario = await Usuario.findById(id);
+
+    res.render('edit.html', {usuario});
+})
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    await Usuario.updateOne({_id: id}, req.body);
+
+    res.redirect('/');
+});
+
+
+module.exports = router;
