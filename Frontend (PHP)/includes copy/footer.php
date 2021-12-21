52af6698833e33d0a59e791684a8fb967b185e6d
Senan <senan996@gmail.com>
2021-12-21 14:33:13 +0100

    Api incidencias cliente

---
+++ Frontend (PHP)/includes copy/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer seccion">
+    <div class="contenedor contenedor-footer">    
+        <nav class="navegacion">
+            <p>Nombres</p>
+        </nav>
+    </div>
+</footer>
+</body>
+</html>
---
+++ Frontend (PHP)/index copy.php
@@ -0,0 +1,47 @@
+<?php
+
+    session_start();
+    $res = file_get_contents("http://localhost:3000/");
+    $data = json_decode($res);
+
+    if(isset($_SESSION['server_msg'])){
+        echo $_SESSION['server_msg'];
+        unset($_SESSION['server_msg']);
+    }
+
+    include './includes/header.php';
+?>
+
+
+
+    <div class="header-opciones">
+        <a href="usuario/crear_usuario.php" class="btn btn-primary">Crear usuario</a>
+    </div>
+
+    <table>
+        <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+        </tr>
+            <?php 
+                foreach ($data->data->usuarios as $usuario){ ?>
+                
+                    <tr>
+                        <th><?php echo $usuario->nombre; ?></th>
+                        <th><?php echo $usuario->apellido; ?></th>
+                        <form action="usuario/delete.php" method="POST">
+                            <input type="hidden" value="<?php echo $usuario->_id?>" name="id">
+                            <th><input type="submit" value="Eliminar"></th>
+                        </form>
+                        <form action="usuario/edit.php" method="GET">
+                            <input type="hidden" value="<?php echo $usuario->_id?>" name="id">
+                            <th><input type="submit" value="Editar"></th>
+                        </form>
+                    </tr>
+                
+            <?php } ?>
+    </table>
+
+    <?php include './includes/buscador_incidencias.php' ?>
+
+    <?php include './includes/footer.php' ?>
