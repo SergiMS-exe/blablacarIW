0ea8054be4a0340b68c74d02003dc0492f03b053
Senan <senan996@gmail.com>
2021-12-21 14:34:14 +0100

    Api incidencias cliente

--- Frontend (PHP)/includes copy/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer seccion">
-    <div class="contenedor contenedor-footer">    
-        <nav class="navegacion">
-            <p>Nombres</p>
-        </nav>
-    </div>
-</footer>
-</body>
-</html>
