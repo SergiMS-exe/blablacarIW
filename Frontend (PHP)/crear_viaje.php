77628db8a49b9b25ea6c6be4633725490ec661c4
AaronHDN <62114009+AaronHDN@users.noreply.github.com>
2021-12-21 17:01:00 +0100

    add/edit/delete casi terminado

---
+++ Frontend (PHP)/crear_viaje.php
@@ -0,0 +1,50 @@
+<?php
+    session_start();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $url = 'http://localhost:3000/travels/add';
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+
+        $data = array(
+            "id_pasajeros" => array(),
+            "id_conductor" => $_POST['id_conductor'],
+            "fecha_salida" => $_POST['fecha_salida'],
+            "hora_salida" => $_POST['hora_salida'],            
+            "lugar_salida" => $_POST['lugar_salida'],
+            "lugar_llegada" => $_POST['lugar_llegada']
+        );
+
+        $json = http_build_query($data);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        $output = curl_exec($ch);
+        $info = curl_getinfo($ch);
+        curl_close($ch); 
+        $result = json_decode($output);
+        
+        $_SESSION['server_msg'] = $result->data->msg;
+        
+        header('Location: index.php');
+    }
+?>
+
+
+<h1>Crear viaje</h1>
+
+<form action="crear_viaje.php" method="POST">
+    <input placeholder="fecha_salida" name="fecha_salida">
+    <input placeholder="hora_salida" name="hora_salida">
+    <input placeholder="lugar_salida" name="lugar_salida">
+    <input placeholder="lugar_llegada" name="lugar_llegada">
+    <input placeholder="id_conductor" name="id_conductor">
+    <!-- <input type="hidden" name="id_pasajeros[]" value="61c0ef8108a00e29cc6f9b9c"> -->
+    <input type="submit" value="Crear">
+</form>
+
+
+<a href="index.php" class="btn btn-danger">Cancelar</a>
--- Frontend (PHP)/index.php
+++ Frontend (PHP)/index.php
@@ -2,6 +2,8 @@
     session_start();
     $res = file_get_contents("http://localhost:3000/");
     $data = json_decode($res);
+    $resViajes = file_get_contents("http://localhost:3000/listaviajes");
+    $dataViajes = json_decode($resViajes);
 
     if(isset($_SESSION['server_msg'])){
         echo $_SESSION['server_msg'];
@@ -40,5 +42,44 @@
                 
             <?php } ?>
     </table>
+
+    <div class="header-opciones">
+        <a href="crear_viaje.php" class="btn btn-primary">Crear viaje</a>
+    </div>
+
+    <table>
+        <tr>
+            <th>Conductor</th>
+            <th>Fecha Salida</th>
+            <th>Hora Salida</th>
+            <th>Lugar Salida</th>
+            <th>Lugar Llegada</th>
+        </tr>
+            <?php 
+                foreach ($dataViajes->data->viajes as $viaje){ ?>
+                    <?php
+                        $resAux = file_get_contents("http://localhost:3000/users/edit/".$viaje->id_conductor);
+                        $dataAux = json_decode($resAux);
+                        $conductor = $dataAux->data->usuario[0];
+                    ?>                
+                    <tr>
+                        <th><?php echo $conductor->nombre; ?></th>
+                        <th><?php echo $viaje->fecha_salida; ?></th>
+                        <th><?php echo $viaje->hora_salida; ?></th>
+                        <th><?php echo $viaje->lugar_salida; ?></th>
+                        <th><?php echo $viaje->lugar_llegada; ?></th>
+                        <form action="delete_viaje.php" method="POST">
+                            <input type="hidden" value="<?php echo $viaje->_id?>" name="id">
+                            <th><input type="submit" value="Eliminar"></th>
+                        </form>
+                        <form action="edit_viaje.php" method="GET">
+                            <input type="hidden" value="<?php echo $viaje->_id?>" name="id">
+                            <th><input type="submit" value="Editar"></th>
+                        </form>
+                    </tr>
+                
+            <?php } ?>
+    </table>
+
 
     <?php include './includes/footer.php' ?>
